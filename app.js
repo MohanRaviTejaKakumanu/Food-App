27b4856ba77f10319078d45da65279ac50fd2923
MohanRaviTeja <mohanravitejakakumanu@gmail.com>
2025-03-16 16:51:35 +0530

    intial comment

---
+++ app.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const AppLayout = () => {
+  return <div>Hey, Just Created React App</div>;
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
